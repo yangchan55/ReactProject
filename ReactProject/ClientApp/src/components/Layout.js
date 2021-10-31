408d84be8d21637d668b42ea1f4247c8d821a199
sungeunsong <pleasant6772@gmail.com>
2021-11-01 08:17:15 +0900

    프론트 기본 틀 설정

--- ReactProject/ClientApp/src/components/Layout.js
+++ ReactProject/ClientApp/src/components/Layout.js
@@ -1,18 +1,61 @@
-import React, { Component } from 'react';
-import { Container } from 'reactstrap';
-import { NavMenu } from './NavMenu';
+import React, { useState } from 'react';
+import { Layout, Menu } from 'antd';
+import {
+	MenuUnfoldOutlined,
+	MenuFoldOutlined,
+	UserOutlined,
+	VideoCameraOutlined,
+	UploadOutlined,
+} from '@ant-design/icons';
 
-export class Layout extends Component {
-  static displayName = Layout.name;
+const { Header, Sider, Content } = Layout;
 
-  render () {
-    return (
-      <div>
-        <NavMenu />
-        <Container>
-          {this.props.children}
-        </Container>
-      </div>
-    );
-  }
-}
+export default () => {
+	const [state, setState] = useState({ collapsed: false });
+
+	const toggle = () => {
+		setState({
+			collapsed: !state.collapsed,
+		});
+	};
+
+	return (
+		<Layout style={{ minHeight: '100vh' }}>
+			<Sider trigger={null} collapsible collapsed={state.collapsed}>
+				<div className='logo' />
+				<Menu theme='dark' mode='inline' defaultSelectedKeys={['1']}>
+					<Menu.Item key='1' icon={<UserOutlined />}>
+						nav 1
+					</Menu.Item>
+					<Menu.Item key='2' icon={<VideoCameraOutlined />}>
+						nav 2
+					</Menu.Item>
+					<Menu.Item key='3' icon={<UploadOutlined />}>
+						nav 3
+					</Menu.Item>
+				</Menu>
+			</Sider>
+			<Layout className='site-layout'>
+				<Header className='site-layout-background' style={{ padding: 0 }}>
+					{React.createElement(
+						state.collapsed ? MenuUnfoldOutlined : MenuFoldOutlined,
+						{
+							className: 'trigger nav-trigger',
+							onClick: toggle,
+						}
+					)}
+				</Header>
+				<Content
+					className='site-layout-background'
+					style={{
+						margin: '24px 16px',
+						padding: 24,
+						minHeight: 280,
+					}}
+				>
+					Content
+				</Content>
+			</Layout>
+		</Layout>
+	);
+};
